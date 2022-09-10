fa98746c6c205562a3a53b03cf6b3dfc6b435860
diego <admin@dbltecnologia.com.br>
2022-09-10 14:44:43 -0300

    Versão 1.0 Testing

--- scripts/refresh_results.sh
+++ scripts/refresh_results.sh
@@ -2,9 +2,9 @@
 
 
 
-#rm -rf /tmp/bet/
-#mkdir /tmp/bet/
-#/home/diego/Documentos/Projetos/Ghost/Ghost-Bet/scripts/save_page_as.sh "https://www.flashscore.com.br" --browser "firefox" --destination "/tmp/bet/bet.html" --load-wait-time 5
+rm -rf /tmp/bet/
+mkdir /tmp/bet/
+/home/diego/Documentos/Projetos/Ghost/Ghost-Bet/scripts/save_page_as.sh "https://www.flashscore.com.br" --browser "firefox" --destination "/tmp/bet/bet.html" --load-wait-time 5
 
 python ./uteis/results.py
 
